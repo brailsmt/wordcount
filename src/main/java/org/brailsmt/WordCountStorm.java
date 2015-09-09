7292ff4e2eb7b14cb329e35cb350046f5e070c64
Michael Brailsford <michael.brailsford@cerner.com>
2015-09-09 17:16:36 -0500

    wip

---
+++ src/main/java/org/brailsmt/WordCountStorm.java
@@ -0,0 +1,40 @@
+package org.brailsmt;
+
+/*
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+*/
+
+public class WordCountStorm /* extends BaseRichBolt */ {
+    /*
+    
+    private static final long serialVersionUID = 1864133374708619390L;
+
+    private Map stormConf;
+    private TopologyContext topologyContext;
+    private OutputCollector outputCollector;
+
+    public WordCountStorm() {
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.stormConf       = stormConf;
+        this.topologyContext = context;
+        this.outputCollector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    }
+    */
+}
